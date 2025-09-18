5d634a356e9d39035346a90a7a1234c2d389889a
Hasan Huseyin Yakut <hasanhuseyinyakut@gmail.com>
2025-09-18 03:36:25 +0300

    fix

--- RubyElektronik/obj/Debug/net8.0/RubyElektronik.AssemblyInfo.cs
+++ RubyElektronik/obj/Debug/net8.0/RubyElektronik.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RubyElektronik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8181230a4febddd77d27434cde04329151e231f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3a50bd256672b79dec75a3f3764f94c9541db9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RubyElektronik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RubyElektronik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
